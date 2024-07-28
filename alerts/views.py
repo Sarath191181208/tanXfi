e6b3ee49a82acf54ada48e030fb9a78be7f473ba
Sarath191181208 <vssarathc04@gmail.com>
2024-07-29 02:58:50 +0530

    updated views py file

--- alerts/views.py
+++ alerts/views.py
@@ -1,57 +1,51 @@
-from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .models import Alert
+
 from .serializers import AlertSerializer
-import redis
-
-# Setup Redis
-# cache = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
-
-class Cache:
-    def __init__(self) -> None:
-        ...
-    def get(self, _: str):
-        return None 
-    def set(self, timeout: int, *_):
-        return None
-cache = Cache()
+from .models import Alert
 
 
 class AlertCreateView(generics.CreateAPIView):
+    # serialize the incoming data to Alert model
     serializer_class = AlertSerializer
+    # authorize the user to create an alert
     permission_classes = [IsAuthenticated]
-
+    # Save the user who created the alert
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
 
 class AlertDeleteView(generics.DestroyAPIView):
     serializer_class = AlertSerializer
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        obj = Alert.objects.get(id=self.kwargs['pk'], user=self.request.user)
-        obj.status = 'deleted'
+        obj = Alert.objects.get(id=self.kwargs["pk"], user=self.request.user)
+        obj.status = "deleted"
         obj.save()
+        # return the alert object
         return obj
+
 
 class AlertListView(generics.ListAPIView):
     serializer_class = AlertSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        status_filter = self.request.query_params.get('status')
-        queryset = Alert.objects.filter(user=self.request.user)
+        status_filter = self.request.query_params.get("status")
         if status_filter:
-            queryset = queryset.filter(status=status_filter)
+            queryset = Alert.objects.filter(
+                user=self.request.user, status=status_filter
+            )
+        else:
+            queryset = Alert.objects.filter(user=self.request.user)
         return queryset
 
     def list(self, request, *args, **kwargs):
-        cache_key = f"alerts_{request.user.id}_{request.query_params.get('status', 'all')}"
-        cached_data = cache.get(cache_key)
-        if cached_data:
-            return Response(cached_data)
+        # cache_key = f"alerts_{request.user.id}_{request.query_params.get('status', 'all')}"
+        # cached_data = cache.get(cache_key)
+        # if cached_data:
+        #     return Response(cached_data)
         response = super().list(request, *args, **kwargs)
         # cache.set(cache_key, response.data, timeout=60*5)  # Cache for 5 minutes
         return response
