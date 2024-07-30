ef72e5a64ef6de6b651f1f47ed08a586de672a50
Sarath191181208 <vssarathc04@gmail.com>
2024-07-30 23:25:15 +0530

    migrated to postgres

--- price_alert/settings.py
+++ price_alert/settings.py
@@ -89,9 +89,13 @@ WSGI_APPLICATION = "price_alert.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tanxfi', 
+        'USER': 'user',
+        'PASSWORD': 'psswd',
+        'HOST': 'db', 
+        'PORT': '5432',
     }
 }
 
